6067eea5e16e2007d99fcc8fd2de543e76373f75
Boonma Vue <bvue006@student.butte.edu>
2016-08-25 21:29:26 +0000

    Ascii art updated

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "         ////////    " << endl;
+cout << "       ///////////   " << endl;
+cout << "     //////////////  " << endl;   
+cout << "   ///////////////// " << endl;
+cout << "   ///  O    O  //   " << endl;
+cout << "   //     ,    //    " << endl;
+cout << "  //    __    //     " << endl;
+cout << " //          //      " << endl;
+
+
+
+return 0;
+}
+
