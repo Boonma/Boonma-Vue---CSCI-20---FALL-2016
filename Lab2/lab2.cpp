456836ea773e0eb50d7352cf0ef735ec8f356b8a
Boonma Vue <bvue006@student.butte.edu>
2016-08-28 18:54:18 +0000

    Lab2 submission

---
+++ Lab2/lab2.cpp
@@ -0,0 +1,7 @@
+// Welcome to Boonma's guessing game.
+
+/* Rules: The Computer will attack you for a certain amount of damage between 1-100. Choose the correct number within 10 tries or
+		game over. If chosen incorrectly, the computer will either raise his hands or lower them indicating if the number is higher
+		or lower. Every correct guess gives you an artifical apple. */
+		
+// Congratulation on those apples! 
---
+++ Lab2/praticelab2.cpp
@@ -0,0 +1,27 @@
+// Welcome to Boonma's guessing game.
+
+/* Rules: The Computer will attack you for a certain amount of damage between 1-100. Choose the correct number within 10 tries or
+		game over. If chosen incorrectly, the computer will either raise his hands or lower them indicating if the number is higher
+		or lower. Every correct guess gives you an artifical apple. */
+		
+// Congratulation on those apples!
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numComputer    = 75;
+	int guess              ;
+	int resets             ;
+
+	cout << "Pick a number between 1 through 100:";
+	cin >> guess;
+    cout << numComputer << " is the answer." << endl;
+    cout << "numComputer " << numComputer << " guess" << endl;
+
+	   
+	 return 0;
+}
+
+ 
